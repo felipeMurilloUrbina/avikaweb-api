5adba4f53df780de6868d0fa357cce65c5572da2
felipeMurilloUrbina <40193021+felipeMurilloUrbina@users.noreply.github.com>
2018-07-13 00:02:21 -0600

    cambios 13.07
    Se agrega seguridad, se agrega swagger config para ayuda, se agrega owin para la creacion de usuarios.

---
+++ Avika.Forum/Avika.Forum.WebApiAuthorization/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Avika.Forum.WebApiAuthorization.Models
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+        public string  RoleId { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+        public string Language { get; set; }
+        public bool Active { get; set; }
+        public string Avatar { get; set; }
+        [ForeignKey("RoleId")]
+        public IdentityRole Role { get; set; }
+
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
---
+++ Avika.Forum/Avika.Forum.WebApiAuthorization/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using Avika.Forum.WebApiAuthorization.Formats;
+using System.Web.Http;
+using Avika.Forum.WebApiAuthorization.Providers;
+using Avika.Forum.WebApiAuthorization.Infrastructure;
+using Avika.Forum.WebApiAuthorization.Models;
+
+[assembly: OwinStartup(typeof(Avika.Forum.WebApiAuthorization.Startup))]
+
+namespace Avika.Forum.WebApiAuthorization
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            ConfigureOAuth(app);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                //For Dev enviroment only (on production should be AllowInsecureHttp = false)
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/oauth2/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat("http://jwtauthzsrv.azurewebsites.net")
+            };
+
+            // OAuth 2.0 Bearer Access Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+
+        }
+    }
+}
